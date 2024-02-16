c852b32f902712c04cb52d028d4535ff6d74f818
prashanth-reachvel <158998133+prashanth-reachvel@users.noreply.github.com>
2024-02-16 13:09:39 +0530

    Config Vercel

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,9 @@ const InventoryUpdate = require("./models/UpdateInventory");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // Parse form data
-app.use(cors());
+// app.use(cors());
+// Allow requests from school-module.vercel.app
+app.use(cors({ origin: "https://school-module.vercel.app" }));
 
 require("dotenv").config();
 
