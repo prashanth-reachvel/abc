86986fa1be1214e4ddb1e8592ca2a1428931bbe3
prashanth-reachvel <158998133+prashanth-reachvel@users.noreply.github.com>
2024-02-17 19:11:46 +0530

    Update Requests.jsx

--- frontend/src/components/Requests/Requests.jsx
+++ frontend/src/components/Requests/Requests.jsx
@@ -11,7 +11,7 @@ const Requests = () => {
     const fetchData = async () => {
       try {
         const response = await axios.get(
-          "https://module-api.vercel.app/api/requests?limit=5"
+          "https://mernbackendapp-4hcd.onrender.com/api/requests?limit=5"
         );
         setRequests(response.data);
       } catch (error) {
