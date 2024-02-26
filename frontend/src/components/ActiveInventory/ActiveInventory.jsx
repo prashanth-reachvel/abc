17433e7126f04bfd7c6ba4ce5f56370e1d088045
prashanth-reachvel <158998133+prashanth-reachvel@users.noreply.github.com>
2024-02-26 12:07:55 +0530

    Update ActiveInventory.jsx

--- frontend/src/components/ActiveInventory/ActiveInventory.jsx
+++ frontend/src/components/ActiveInventory/ActiveInventory.jsx
@@ -1,31 +1,29 @@
 import React, { useState, useEffect } from "react";
 import "./ActiveInventory.css";
-import Sidebar from "../Sidebar/Sidebar";
-import Requests from "../Requests/Requests";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
 const ActiveInventory = () => {
   const schoolName = localStorage.getItem("schoolName");
-  const [inventoryData, setInventoryData] = useState(new Map());
+  const [inventoryData, setInventoryData] = useState([]);
+  const [titles, setTitles] = useState([]);
 
   useEffect(() => {
     const fetchInventoryData = async () => {
       try {
         const response = await axios.get(
-          `https://schoolapi.sevabharath.com/api/inventory/${encodeURIComponent(
+          `http://localhost:3000/api/inventory/${encodeURIComponent(
             schoolName
           )}`
         );
         console.log(response.data);
+        setInventoryData(response.data);
 
-        // Update the inventory data map
-        const newInventoryData = new Map(inventoryData);
-        response.data.forEach((item) => {
-          newInventoryData.set(item.title, item);
-        });
-
-        setInventoryData(newInventoryData);
+        // Extract and store unique titles
+        const uniqueTitles = Array.from(
+          new Set(response.data.map((item) => item.title.trim()))
+        );
+        setTitles(uniqueTitles);
       } catch (error) {
         console.error("Error fetching inventory data:", error);
       }
@@ -33,9 +31,11 @@ const ActiveInventory = () => {
     fetchInventoryData();
   }, [schoolName]);
 
+  // console.log(inventoryData);
+  // console.log(titles);
+
   return (
     <div className="active-inventory-main-container">
-      {/* <Sidebar /> */}
       <div className="inventory-box">
         <div className="inventory-heading-btn-container">
           <h3 className="inventory-heading">Active Inventory</h3>
@@ -44,29 +44,41 @@ const ActiveInventory = () => {
           </Link>
         </div>
         <div className="membership-cards">
-          {[...inventoryData.values()].map((item, index) => (
-            <div className="member-green-card" key={index}>
-              <p className="menber-plan-head">{item.title}</p>
-              <div className="white-inner-box">
-                <p className="school-green-text">
-                  Distributed : {item.distributed}
-                </p>
-                <p className="school-green-text">
-                  Available : {item.available}
-                </p>
-                <p className="school-green-text">
-                  Total : {item.totalAddQuantity}
-                </p>
+          {titles.map((title, index) => {
+            const items = inventoryData
+              .filter((item) => item.title === title)
+              .sort(
+                (a, b) => new Date(b.updatedDate) - new Date(a.updatedDate)
+              );
+            if (items.length === 0) {
+              return null;
+            }
+
+            const item = items[0]; // Selecting the latest item
+            return (
+              <div className="member-green-card" key={index}>
+                <p className="menber-plan-head">{item.title}</p>
+                <div className="white-inner-box">
+                  <p className="school-green-text">
+                    Distributed : {item.distributed}
+                  </p>
+                  <p className="school-green-text">
+                    Available : {item.available}
+                  </p>
+                  <p className="school-green-text">
+                    Total : {item.totalAddQuantity}
+                  </p>
+                </div>
+                <Link
+                  to={`/update-inventory/${encodeURIComponent(
+                    item.school
+                  )}/${encodeURIComponent(item.title)}`}
+                >
+                  <button className="add-menber-btn">Update</button>
+                </Link>
               </div>
-              <Link
-                to={`/update-inventory/${encodeURIComponent(
-                  item.school
-                )}/${encodeURIComponent(item.title)}`}
-              >
-                <button className="add-menber-btn">Update</button>
-              </Link>
-            </div>
-          ))}
+            );
+          })}
         </div>
       </div>
     </div>
